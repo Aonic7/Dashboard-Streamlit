00877ad2e6e93475aef558f250c3be2e55d04d32
Reda Farid <Reda@01129290386>
2022-03-06 23:34:43 +0100

    NN Regressor addition

---
+++ Regression/MLP_Regressor.py
@@ -0,0 +1,231 @@
+from cProfile import label
+from collections import namedtuple
+from lib2to3.pgen2.pgen import DFAState
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn 
+import seaborn as sn
+from sklearn import model_selection
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,r2_score
+from typing import NamedTuple
+from numpy.core.fromnumeric import shape, size
+import streamlit as st #streamlit backend
+class NN_Regressor:
+    
+    
+
+    y_pred:                     float # resulting output
+    y_actual:                   float # expected output
+    length:                     int   # length of y_test
+    mean_squared_error:         float # mean square error
+    R2:                         float #R2 Score
+    Train_score:                float
+    X_test:                     float
+    test_score:                 float
+    model:                      MLPRegressor
+    Error_message:              str
+    flag:                       bool
+
+    #coeff:                     tuple
+
+    dependant_var_index =0
+    #NN_Inputs = Regressor_Inputs
+    # = Regressor_Outputs
+    flag=False
+    Error_message='No Error occurred in the processing'
+    
+    #Constructor
+    #External inputs are the Data frame object, the Named Tuple of NN_Inputs and the index of the dependant variable in the Data frame
+    def __init__(self,df,NN_Inputs,dependant_var_index):
+        self.df=df
+        self.NN_Inputs=NN_Inputs
+        
+        self.k=dependant_var_index
+        self.handle()
+
+    #Data handling method, creates the X and Y arrays that go into the Train_test_split method
+    #Called when the object is instantiated within the constructor 
+    def handle(self):
+        try:   
+            self.internal_data =self.df.drop(self.df.iloc[:,[self.k]],axis=1)
+            nxm=shape(self.internal_data)
+            n=nxm[0]
+            m=nxm[1]
+      
+            self.X=np.ndarray(shape=(n,m),dtype=float, order='F')
+            
+
+            for l in range(0,m):  
+            
+                for i in range(0,n):
+                
+                    self.X[i,l]=self.internal_data.iloc[i,l]
+            
+            Y=np.ndarray(shape=(n,1), dtype=float, order='F')
+            Y[:,0]=self.df.iloc[:,self.k]
+
+            self.y=np.ravel(Y)
+        except Exception as e:
+            self.Error_message='Error in Handling Method: ' + str(e)
+            self.flag=True
+        
+       
+    #Method that creates the MLP Regressor and returns the Named Tuple of to be used in other methods  
+    def Regressor(self):
+        
+        if (self.flag) !=True:
+            try:
+                X_train, self.X_test, y_train, self.y_actual= train_test_split(self.X,self.y,test_size=self.NN_Inputs.test_size,shuffle=False, random_state=40)
+                self.model = MLPRegressor(hidden_layer_sizes = self.NN_Inputs.hidden_layers, 
+                                    activation = self.NN_Inputs.activation_fun, solver = self.NN_Inputs.solver_fun, 
+                                    learning_rate = 'adaptive', max_iter = self.NN_Inputs.Max_iterations, random_state = 109,shuffle=True )
+
+
+                self.model.fit(X_train,y_train)
+                self.Train_score= self.model.score(X_train,y_train)
+                self.test_score= self.model.score(self.X_test,self.y_actual)
+                #self.coeff=self.model.coefs_
+
+                
+                self.y_pred = self.model.predict(self.X_test)
+
+                y_pred = np.ndarray.tolist(self.y_pred)
+                self.length = len(y_pred)
+                
+                #Mean squared error and accuracy
+                self.mean_squared_error = mean_squared_error(self.y_actual, 
+                self.y_pred)
+                self.R2=r2_score(self.y_actual, self.y_pred)
+            except Exception as e:
+                self.Error_message= 'Error in Regressor Creation: ' + str(e)
+                self.flag=True
+                self.Train_score= 'Refer To error in Regressor Creation'
+                self.test_score= 'Refer To error in Regressor Creation'
+                #self.coeff=self.model.coefs_
+
+                self.y_actual='Refer To error in Regressor Creation'
+                self.y_pred = 'Refer To error in Regressor Creation'
+
+                self.y_pred = 'Refer To error in Regressor Creation'
+                self.length = 'Refer To error in Regressor Creation'
+                
+                #Mean squared error and accuracy
+                self.mean_squared_error = 'Refer To error in Regressor Creation'
+                self.R2 = 'Refer To error in Regressor Creation'
+        else:
+            self.Train_score= 'Refer To error in Handling Method'
+            self.test_score= 'Refer To error in Handling Method'
+            #self.coeff=self.model.coefs_
+
+            self.y_actual='Refer To error in Handling Method'
+            self.y_pred = 'Refer To error in Handling Method'
+
+            self.y_pred = 'Refer To error in Handling Method'
+            self.length = 'Refer To error in Handling Method'
+            
+            #Mean squared error and accuracy
+            self.mean_squared_error = 'Refer To error in Handling Method'
+            self.R2 = 'Refer To error in Handling Method'
+
+
+        return self
+        
+    def printing(self):
+        #Printing the chosen metrics
+        if (self.flag) != True:
+            self.Error_message= ' No Error Occurred during prcoessing of the code'
+        
+        try:
+            
+
+            # print('Error Message           ', self.Error_message)
+            # print('expected output:        ', self.y_actual)
+            # print('Predicted Output:       ', self.y_pred)
+            # print('Model Train_score on the Training Data: ',  self.Train_score)
+            # print('Model Train_score on the Testing Data:  ',  self.test_score)
+            # print('Mean Square error:      ',  self.mean_squared_error)
+            # print('length of output array: ',  self.length)
+            st.warning(self.Error_message)
+            cc1, cc2 = st.columns(2)
+            with cc1:
+                # st.metric('Expected output:        ', self.NN_Outputs.y_actual)
+                # st.write('Predicted Output:       ', self.NN_Outputs.y_pred)
+                st.metric('Model score on the Training Data:',  round(self.Train_score, 8))
+                st.metric('Model score on the Testing Data:',  round(self.test_score, 8))
+                st.metric('Length of output array: ',  self.length)
+            with cc2:
+                #st.write('Classification Report: ')
+                st.metric('RMSE: ',  round((self.mean_squared_error)**(0.5),8))
+                st.metric('R2 Score:', round(self.R2,8))
+        except Exception as e:
+            self.Error_message = 'Error while printing outputs: ' +str(e)
+            self.flag=True
+            st.warning(self.Error_message)
+        #print('coeff: ',.coeff )
+
+    def plotting(self):
+        #Plotting a 2D plot of the Regression output with external input i being the field of which we want to plot as x-axis
+        if self.flag != True:
+            try:
+                # fig = plt.figure()
+                # ax1 = fig.add_subplot(111)
+
+                # ax1.scatter(self.X_test[:,i],self.y_pred, s=10, c='r', marker="o", label='Predicted')
+                # ax1.scatter(self.X_test[:,i],self.y_actual, s=10, c='b', marker="o", label='actual')
+                # ax1.set_xlabel (self.df.columns[i])
+                # ax1.set_ylabel  (self.df.columns[self.k])   
+                # ax1.legend()
+                # plt.show()
+                df=pd.DataFrame({'y_test':self.y_actual,'y_pred':self.y_pred})
+                fig = plt.figure(figsize=(10, 4))
+                sn.scatterplot(x='y_test',y='y_pred',data=df,label='Real Prediction')
+                sn.lineplot(x='y_test',y='y_test',data=df,color='red',alpha=0.5,label='Optimal Prediction')
+                plt.title('y_test vs y_pred')
+                plt.legend()
+                #plt.show()
+                st.pyplot(fig)
+            except Exception as e:
+                self.Error_message='Error in Plotting Method: ' + str(e)
+                self.flag=True
+                #print(e)
+
+
+#Using the Class
+data = pd.read_csv("D:\MAIT\OOP\Datasets\Regression\synchronous_machine.csv",delimiter=';',decimal=',')
+#print(data.head)
+activation_fun1 = ("identity", "logistic", "tanh", "relu")
+solver_fun1 = ("lbfgs", "sgd", "adam")
+
+hidden_layers3=(600,300,100)
+
+class Regressor_Inputs(NamedTuple):
+        
+        test_size:              float  # test size percentage
+        activation_fun:         tuple  # activation function selection
+        hidden_layers:          tuple  # size of hidden layer and number of neurons in each layer
+        solver_fun:             tuple  # solver function
+        Max_iterations:         int    # number of iterations
+
+#Creating a tuple that will work with the method defined inside the class
+Inputs= Regressor_Inputs(0.2,activation_fun1[3],hidden_layers3,solver_fun1[0],500)
+#print(Inputs)
+
+Regressor1=NN_Regressor(data,Inputs,4)
+
+#print(Regressor1.NN_Inputs)
+#Calling the Regression method that would create the Neural Network
+Regressor1.Regressor()
+
+#input the index of the field that you want to occupy the x axis 
+Regressor1.plotting()
+#Printing the metrics chosen for the Regressor
+Regressor1.printing()
+
+
+#print(NN_Regressor.Error_message)
--- Regression/regression.py
+++ Regression/regression.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from io import StringIO
 from pandas.api.types import is_numeric_dtype
 from .Regression_final import Regressor
+from .MLP_Regressor import NN_Regressor,Regressor_Inputs
 
 def import_dset(data_obj):
     try:
@@ -34,7 +35,7 @@ def main(data_obj):
 
     # Main data classification method radio selector
     rg_method = st.radio(label='Regression Method', options=[
-                         'Random Forest', 'Button 1', 'Button 2'])
+                         'Random Forest', 'Neural Network', 'Button 2'])
 
     # Selected 'Remove outliers'
     if rg_method == 'Random Forest':
@@ -66,6 +67,61 @@ def main(data_obj):
             reg_inst.model(tree_size, tt_proportion)
             reg_inst.result(reg_inst.Y_test, reg_inst.Y_pred)
             reg_inst.prediction_plot(reg_inst.Y_test, reg_inst.Y_pred)
+
+    if rg_method == 'Neural Network':
+
+        st.dataframe(rg_df)
+        st.write(rg_df.shape)
+
+        with st.container():
+            st.subheader('Select input settings')
+
+            cc1, cc2, cc3 = st.columns(3)
+            
+            with cc1:
+                tt_proportion = st.slider('Portion of test data', 0.0, 1.0, 0.2, 0.05)
+
+                iteration_num = st.slider('Number of iterations', 100, 500, 200, 50)
+
+                #norm_bool = st.select_slider('Normalize data?', [False, True], False)              
+                
+            
+            with cc2:
+                columns_list = list(rg_df.columns)
+                selected_column = st.selectbox("Column to Predict:", columns_list)
+                col_idx = rg_df.columns.get_loc(selected_column)
+
+                solver_fun1 = ("lbfgs", "sgd", "adam")
+                selected_solver = st.selectbox("Solver:", solver_fun1)
+
+                activation_fun1 = ("identity", "logistic", "tanh", "relu")
+                selected_function = st.selectbox("Activation function:", activation_fun1)           
+            
+            with cc3:
+                number_hl = st.slider('Hidden layers:', 1, 5, 2, 1)
+
+                a = []
+
+                for i in range(number_hl):
+                    a.append(st.number_input(f'Number of neurons in hidden layer {i+1}:', 1, 20, 1, 1, key=i))
+
+        with st.container():
+            NN_inputs = Regressor_Inputs(tt_proportion,
+                                       selected_function,
+                                       tuple(a),
+                                       selected_solver,
+                                       iteration_num
+                                       #norm_bool
+                                       )
+            st.write(type(tt_proportion))
+
+            MLP_Regressor = NN_Regressor(rg_df, NN_inputs, col_idx)
+
+            MLP_Regressor.Regressor()
+            # st.write(Classifier.Classify()[52])
+            MLP_Regressor.printing()
+            
+            MLP_Regressor.plotting()
 
 if __name__ == "__main__":
    main()
